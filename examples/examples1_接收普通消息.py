02d1f95fff94f7436ed8d40c4370f162d62d8002
wsu2059q <wsu2059@qq.com>
2025-06-09 09:39:15 +0800

    examples: 修正demo

--- examples/examples1_接收普通消息.py
+++ examples/examples1_接收普通消息.py
@@ -3,7 +3,7 @@ sdk.init("你的机器人token")# 替换为你自己的token
 bot = BOT()
 # 在此段代码中，发送消息将返回"收到你的消息啦！"。当发送"ping"时，将同时返回"pong"
 def deliver(data):
-    if data.event == "message.receive.normal"：
+    if data.event == "message.receive.normal":
         bot.send.text(data.id, data.type, "收到你的消息了啦！")
         if data.msg == "ping":
             bot.send.text(data.id, data.type, "pong")
